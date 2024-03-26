d0ef3e37b14a9160803c67b20c65936c6931a54c
nightwhite <nightwhite634@gmail.com>
2024-03-27 04:10:21 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/zsh
 
+# 安装全局 Node.js 命令
+npm install -g pnpm --registry=https://registry.npmmirror.com
+npm install -g laf-cli --registry=https://registry.npmmirror.com
+
 # 安装中文语言包
 code-server --install-extension ms-ceintl.vscode-language-pack-zh-hans
 
 code-server
+
